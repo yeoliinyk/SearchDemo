1225e8eb3721940ec04869c597601f01197f33c5
Yevgen Oliinykov <zhekin08@gmail.com>
2018-03-14 19:39:54 +0200

    Missing constructor.

--- app/src/main/java/my/company/com/searchdemo/domain/models/Movie.java
+++ app/src/main/java/my/company/com/searchdemo/domain/models/Movie.java
@@ -10,6 +10,12 @@ public class Movie {
     private String name;
     private String description;
 
+    public Movie(long id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
     public long getId() {
         return id;
     }
